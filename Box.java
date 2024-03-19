cca8652b81ae27e7f94b31501c8c599ac0721257
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-19 08:28:35 +0300

    Addded methods: addFruit, removeFruit, moveFruit

--- Box.java
+++ Box.java
@@ -8,4 +8,18 @@ public class Box<T extends Fruit> {
         fruits = new ArrayList<>();
     }
 
+    public void addFruit(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public void removeFruit(T fruit) {
+        fruits.remove(fruit);
+    }
+
+    public void moveFruit(Box<T> box2) {
+        box2.fruits.addAll(fruits);
+        fruits.clear();
+    }
+
+
 }
