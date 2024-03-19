cce8448f3f5a7e0eba2dfe407a56a70b003af88d
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-19 08:20:24 +0300

    Addded pole, getter and constructor

--- Fruit.java
+++ Fruit.java
@@ -1,4 +1,17 @@
 
-public class Fruit {
+public abstract class Fruit {
+
+    /* 
+     * Вес фрукта
+    */
+    private float weight;
+    
+    public float getWeight() {
+        return weight;
+    }
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
 
 }
